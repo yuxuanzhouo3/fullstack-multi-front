74a09058df6ebbddbf2300c1d65860376095a014
yzcmf <yzcmf@example.com>
2025-07-12 17:23:28 -0400

    Init db + redis

--- shared/utils/redis.js
+++ shared/utils/redis.js
@@ -1,12 +1,6 @@
-export const redisGet = async (key) => {
-  const res = await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/get/${key}`, {
-    headers: { Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}` },
-  });
-  return (await res.json()).result;
-};
+import { Redis } from '@upstash/redis';
 
-export const redisSet = async (key, value) => {
-  await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/set/${key}/${value}`, {
-    headers: { Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}` },
-  });
-}; 
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL || 'https://firm-minnow-40919.upstash.io',
+  token: process.env.UPSTASH_REDIS_REST_TOKEN || 'AZ_XAAIjcDEwOWFkMDEwOTFjMTE0YTdjYWY5MTE3OWNlNGQ0MWQxNHAxMA',
+}); 
